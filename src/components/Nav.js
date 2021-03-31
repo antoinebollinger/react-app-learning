dff00e3778b5f94eac70f43b587d444d5c2f105c
Antoine Bollinger <antoine.bollinger@gmail.com>
2021-03-31 16:28:22 +0200

    responsive without jquery using state

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,23 +4,29 @@ import ScrollspyNav from "react-scrollspy-nav";
 import LanguageSelector from '../services/languageSelector';
 
 import { useTranslation } from "react-i18next";
+import { useState } from "react";
 
 const Nav = () => {
     const { t } = useTranslation();
     const refreshPage = () => {
         window.location.reload();
     }
+    const [isActive, setActive] = useState(false);
+    const toggleClass = () => {
+        setActive(!isActive);
+    };
 
+   
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="sideNav">
-            <p className="navbar-brand" onClick={() => refreshPage()}>
+            <p className="navbar-brand m-0" onClick={() => refreshPage()}>
                 <span className="d-block d-lg-none subheading">Antoine Bollinger</span>
                 <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={profile} alt="Accueil" /></span>
             </p>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onClick={() => toggleClass()}><span className="navbar-toggler-icon"></span></button>
+            <div className={`collapse navbar-collapse ${(isActive) ? 'show' : ''}`} id="navbarSupportedContent">
                 <ScrollspyNav scrollTargetIds={['about', 'skills', 'portofolio', 'experience', 'education']} activeNavClass="active" scrollDuration="400">
-                    <ul className="navbar-nav">
+                    <ul className="navbar-nav" onClick={() => toggleClass()}>
                         <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#about">{ t('nav.about') }</a></li>
                         <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#skills">{ t('nav.skills') }</a></li>
                         <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#portofolio">{ t('nav.projects') }</a></li>
