aa2afcf6ac4bca24fd621d7d5c6a0d3dce77a1a5
Antoine Bollinger <antoine.bollinger@gmail.com>
2021-04-02 08:31:09 +0200

    flex-shrink-0

--- src/sections/Experience.js
+++ src/sections/Experience.js
@@ -7,7 +7,7 @@ const Experience = ({ t }) => {
             <div key={exp.class} className="d-flex flex-column-reverse flex-md-row justify-content-between mb-5">
                 <div className="flex-grow-1">
                     <div className="d-flex flex-row align-items-md-center mb-3">
-                        <div className={`resume-section-image mr-3 ${ exp.class}`}></div>
+                        <div className={`resume-section-image mr-3 flex-shrink-0 ${ exp.class}`}></div>
                         <div className="flex-grow-1">
                             <h3 className="mb-0">{ exp.title }</h3>
                             <div className="subheading">{ exp.company }</div>
